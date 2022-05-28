487eb6c8d2e0d2326db0a0f679343278fd4f3325
genwatt <adriankosoglos@gmail.com>
2022-05-28 21:26:00 +0200

    add tabs

---
+++ src/views/Playlist.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Playlist</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/views/Songs.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Songs</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
